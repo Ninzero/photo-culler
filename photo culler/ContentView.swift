29d53cf7544a81a0e499c165d674282b2f376a68
Ninzero Xu <xuts45@outlook.com>
2026-02-18 21:37:57 +0800

    Initial Commit

---
+++ photo culler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  photo culler
+//
+//  Created by Tiansheng Xu on 2026-02-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ photo culler/photo_cullerApp.swift
@@ -0,0 +1,17 @@
+//
+//  photo_cullerApp.swift
+//  photo culler
+//
+//  Created by Tiansheng Xu on 2026-02-18.
+//
+
+import SwiftUI
+
+@main
+struct photo_cullerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
